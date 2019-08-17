0baff5a8c19587a1b96ca78b7392ccb99e097c89
Jose L. Almada <jalmada.e@gmail.com>
2019-08-17 15:39:48 -0400

    Added Cors and packages

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var cors = require('cors');
 
 app.use(express.static('public'));
+
+var allowedOrigins = ["http://localhost:9000", "http://localhost:3001", "http://localhost:3000"];
+app.use(cors({
+  origin: function(origin, callback){
+
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 io.on('connection', function(socket){
   console.log('a user connected');
